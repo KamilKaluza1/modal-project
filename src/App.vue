4bcbbf08cbf1221e4fc0aaafdaa193ddea64e8bb
Kamil Kałuża <kamil321.94@gmail.com>
2023-02-08 16:43:34 +0100

    done

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <p>Welcome...</p>
+  <teleport to=".modals" v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <h1>Sing up for Giveaway</h1>
+      <p>Grab your ninja swag for 50%</p>
+      <template v-slot:links>
+        <a href="#">sing up now</a>
+        <a href="#">more info</a>
+      </template>
+    </Modal>
+  </teleport>
+  <button @click="toggleModal">Open Modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Modal,
+  },
+  data() {
+    return {
+      title: "My vue app",
+
+      showModal: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
+#app, .modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  color: #2c3e50; 
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="backdrop" @click.self="close">
+        <div class="modal" :class="{sale: theme === 'sale'}">
+            <slot></slot>
+            <div class="actions">
+                <slot name="links"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    props: ['theme'],
+    methods: {
+        close() {
+            this.$emit('close')
+        }
+    },
+    data() {
+        return{
+            
+        }
+    }
+}
+</script>
+
+<style>
+.modal{
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+    }
+.backdrop{
+    top: 0;
+    position: fixed;
+    background-color: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+}
+h1{
+    color: #03cfb4;
+    border: none;
+    padding:0;
+}
+
+.modal.sale{
+    background: crimson;
+    color: white;
+}
+
+.modal.sale h1{
+    color:white;
+}
+.modal .actions{
+    text-align: center;
+    margin: 30px 0 10px 0 ;
+}
+.modal .actions a{
+    color: #333;
+    padding: 8px;
+    border: solid 1px #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+}
+.modal .sale .actions{
+    color: white;
+}
+
+.modal .sale .actions a{
+    color: white;
+}
+</style>
